af5cf6745037d94cac109011c7a2e1aad227dee9
Maxime <36773244+Maxouuille@users.noreply.github.com>
2019-06-21 19:15:18 +0200

    ajout model missions

---
+++ src/models/missions/Missions.ts
@@ -0,0 +1,11 @@
+
+export interface Missions {
+    mission_name:  string;
+    mission_id:    string;
+    manufacturers: string[];
+    payload_ids:   string[];
+    wikipedia:     string;
+    website:       string;
+    twitter:       null | string;
+    description:   string;
+}
